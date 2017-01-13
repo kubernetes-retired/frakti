34d82e2a58e5a493353088a79d19d80eebf3e7b8
YaoZengzeng <yaozengzeng@zju.edu.cn>
2017-01-13 18:35:08 +0800

    set flag for cniNetDir and cniPluginDir
    Signed-off-by: Yao Zengzeng <yaozengzeng@zju.edu.cn>

--- pkg/hyper/hyper.go
+++ pkg/hyper/hyper.go
@@ -45,7 +45,7 @@ type Runtime struct {
 }
 
 // NewHyperRuntime creates a new Runtime
-func NewHyperRuntime(hyperEndpoint string, streamingConfig *streaming.Config) (*Runtime, streaming.Server, error) {
+func NewHyperRuntime(hyperEndpoint string, streamingConfig *streaming.Config, cniNetDir string, cniPluginDir string) (*Runtime, streaming.Server, error) {
 	hyperClient, err := NewClient(hyperEndpoint, hyperConnectionTimeout)
 	if err != nil {
 		glog.Fatalf("Initialize hyper client failed: %v", err)
@@ -62,7 +62,7 @@ func NewHyperRuntime(hyperEndpoint string, streamingConfig *streaming.Config) (*
 		}
 	}
 
-	netPlugin, err := ocicni.InitCNI("")
+	netPlugin, err := ocicni.InitCNI(cniNetDir, cniPluginDir)
 	if err != nil {
 		return nil, nil, err
 	}
