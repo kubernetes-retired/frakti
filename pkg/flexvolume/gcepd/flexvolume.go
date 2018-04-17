d393fdd4dfb788b4305070311d3de1a087111880
AdamDang <adam.dang@daocloud.io>
2018-04-17 13:49:57 +0800

    Correct the returned message
    Correct the returned message.

--- pkg/flexvolume/gcepd/flexvolume.go
+++ pkg/flexvolume/gcepd/flexvolume.go
@@ -172,7 +172,7 @@ func detachDiskLogError(d *FlexVolumeDriver) {
 func (d *FlexVolumeDriver) unmount(targetMountDir string) (map[string]interface{}, error) {
 	// check the target directory
 	if _, err := os.Stat(targetMountDir); os.IsNotExist(err) {
-		return nil, fmt.Errorf("volume directory: %v does not exists", targetMountDir)
+		return nil, fmt.Errorf("volume directory: %v does not exist", targetMountDir)
 	}
 
 	//  initialize FlexVolumeDriver manager by reading cinderConfig from metadata file
