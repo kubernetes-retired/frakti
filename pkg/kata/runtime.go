19604deae6a1112939248eefb712a2efda02cfc3
ZeroMagic <anthonyliu@zju.edu.cn>
2018-07-24 14:20:12 +0800

    Add Runtime delete
    Signed-off-by: ZeroMagic <anthonyliu@zju.edu.cn>

--- pkg/kata/runtime.go
+++ pkg/kata/runtime.go
@@ -19,8 +19,11 @@ package kata
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strconv"
+	"time"
 
 	eventstypes "github.com/containerd/containerd/api/events"
 	types "github.com/containerd/containerd/api/types"
@@ -28,6 +31,7 @@ import (
 	identifiers "github.com/containerd/containerd/identifiers"
 	log "github.com/containerd/containerd/log"
 	"github.com/containerd/containerd/metadata"
+	"github.com/containerd/containerd/mount"
 	"github.com/containerd/containerd/namespaces"
 	"github.com/containerd/containerd/platforms"
 	"github.com/containerd/containerd/plugin"
@@ -37,6 +41,10 @@ import (
 	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 	runtimespec "github.com/opencontainers/runtime-spec/specs-go"
 	errors "github.com/pkg/errors"
+
+	"github.com/containerd/containerd/runtime/kata/proc"
+	"github.com/sirupsen/logrus"
+	"golang.org/x/sys/unix"
 )
 
 const (
@@ -89,6 +97,18 @@ func New(ic *plugin.InitContext) (interface{}, error) {
 		events:  ic.Events,
 	}
 
+	tasks, err := r.restoreTasks(ic.Context)
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO: need to add the tasks to the monitor
+	for _, t := range tasks {
+		if err := r.tasks.AddWithNamespace(t.namespace, t); err != nil {
+			return nil, err
+		}
+	}
+
 	log.G(ic.Context).Infoln("Runtime: start containerd-kata plugin")
 
 	// TODO(ZeroMagic): reconnect the existing kata containers
@@ -109,6 +129,8 @@ func (r *Runtime) Create(ctx context.Context, id string, opts runtime.CreateOpts
 	if err != nil {
 		return nil, err
 	}
+
+	log.G(ctx).Infof("Runtime: Namespace is %s\n", namespace)
 
 	if err := identifiers.Validate(id); err != nil {
 		return nil, errors.Wrapf(err, "invalid task id")
@@ -149,29 +171,40 @@ func (r *Runtime) Create(ctx context.Context, id string, opts runtime.CreateOpts
 	}
 	spec := s.(*runtimespec.Spec)
 	containerType := spec.Annotations[annotations.ContainerType]
-	log.G(ctx).Infof("Runtime: ContainerType is %s\n", containerType)
+	log.G(ctx).Infof("Runtime: %s ContainerType is %s", id, containerType)
+	sandboxID := spec.Annotations[annotations.SandboxID]
 
 	// 6. new task. Init the vm, sandbox, and necessary container.
-	t, err := newTask(ctx, id, namespace, pid, r.monitor, r.events, opts, bundle)
+	t, err := newTask(id, namespace, pid, r.monitor, r.events)
 	if err != nil {
 		return nil, err
 	}
+	t.containerType = containerType
+	t.sandboxID = sandboxID
 
+	// 7. create sandbox or container
+	config := &proc.InitConfig{
+		ID:            id,
+		SandboxID:     sandboxID,
+		ContainerType: containerType,
+		Rootfs:        opts.Rootfs,
+		Terminal:      opts.IO.Terminal,
+		Stdin:         opts.IO.Stdin,
+		Stdout:        opts.IO.Stdout,
+		Stderr:        opts.IO.Stderr,
+	}
+	init, err := proc.NewInit(ctx, bundle.path, bundle.workDir, namespace, int(pid), config)
+	if err != nil {
+		return nil, errors.Wrap(err, "new init process error")
+	}
+	t.processList[id] = init
+
+	// 8. add the task to taskList
 	if err := r.tasks.Add(ctx, t); err != nil {
 		return nil, err
 	}
-	// 7. after the task is created, add it to the monitor if it has a cgroup
-	// this can be different on a checkpoint/restore
-	if t.cg != nil {
-		if err = r.monitor.Monitor(t); err != nil {
-			if _, err := r.Delete(ctx, t); err != nil {
-				log.G(ctx).WithError(err).Error("deleting task after failed monitor")
-			}
-			return nil, err
-		}
-	}
 
-	// 8. publish create event
+	// 9. publish create event
 	r.events.Publish(ctx, runtime.TaskCreateEventTopic, &eventstypes.TaskCreate{
 		ContainerID: id,
 		Bundle:      bundle.path,
@@ -202,7 +235,106 @@ func (r *Runtime) Tasks(ctx context.Context) ([]runtime.Task, error) {
 // Delete removes the task in the runtime.
 func (r *Runtime) Delete(ctx context.Context, t runtime.Task) (*runtime.Exit, error) {
 
-	// TODO(ZeroMagic): delete a task
+	// monitor will be handled
 
-	return nil, fmt.Errorf("not implemented")
+	taskID := t.ID()
+	namespace, err := namespaces.NamespaceRequired(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	bundle := loadBundle(
+		taskID,
+		filepath.Join(r.state, namespace, taskID),
+		filepath.Join(r.root, namespace, taskID),
+	)
+
+	// unmount
+	if err := mount.Unmount(filepath.Join(bundle.path, "rootfs"), 0); err != nil {
+		log.G(ctx).WithError(err).WithFields(logrus.Fields{
+			"path": bundle.path,
+			"id":   taskID,
+		}).Warnf("unmount task rootfs")
+	}
+
+	// delete process
+	p := t.(*Task).GetProcess(taskID)
+	if err := p.Delete(ctx); err != nil {
+		return nil, err
+	}
+
+	// remove the task
+	r.tasks.Delete(ctx, taskID)
+
+	// delete the bundle
+	if err := bundle.Delete(); err != nil {
+		log.G(ctx).WithError(err).Error("failed to delete bundle")
+	}
+
+	r.events.Publish(ctx, runtime.TaskDeleteEventTopic, &eventstypes.TaskDelete{
+		ContainerID: taskID,
+		Pid:         uint32(10244),
+		ExitStatus:  128 + uint32(unix.SIGKILL),
+		ExitedAt:    time.Now(),
+	})
+
+	return &runtime.Exit{
+		Pid:       uint32(p.Pid()),
+		Status:    uint32(p.ExitStatus()),
+		Timestamp: time.Now(),
+	}, nil
+}
+
+func (r *Runtime) restoreTasks(ctx context.Context) ([]*Task, error) {
+	dir, err := ioutil.ReadDir(r.state)
+	if err != nil {
+		return nil, err
+	}
+	var o []*Task
+	for _, namespace := range dir {
+		if !namespace.IsDir() {
+			continue
+		}
+		name := namespace.Name()
+		log.G(ctx).WithField("namespace", name).Debug("loading tasks in namespace")
+		tasks, err := r.loadTasks(ctx, name)
+		if err != nil {
+			return nil, err
+		}
+		o = append(o, tasks...)
+	}
+	return o, nil
+}
+
+func (r *Runtime) loadTasks(ctx context.Context, ns string) ([]*Task, error) {
+	dir, err := ioutil.ReadDir(filepath.Join(r.state, ns))
+	if err != nil {
+		return nil, err
+	}
+	var o []*Task
+	for _, path := range dir {
+		if !path.IsDir() {
+			continue
+		}
+		id := path.Name()
+		bundle := loadBundle(
+			id,
+			filepath.Join(r.state, ns, id),
+			filepath.Join(r.root, ns, id),
+		)
+		pidByte, _ := ioutil.ReadFile(filepath.Join(bundle.path, proc.InitPidFile))
+		pidStr := string(pidByte)
+		pid64, err := strconv.ParseInt(pidStr, 10, 32)
+		pid := uint32(pid64)
+
+		ctx = namespaces.WithNamespace(ctx, ns)
+
+		t, err := newTask(id, ns, pid, r.monitor, r.events)
+		if err != nil {
+			log.G(ctx).WithError(err).Error("loading task type")
+			continue
+		}
+		o = append(o, t)
+	}
+	return o, nil
 }
